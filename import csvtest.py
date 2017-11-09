6f183103d5369e087035050b49d48b89ecb8dc1b
boeboska <31690823+boeboska@users.noreply.github.com>
2017-11-09 10:59:43 +0100

    Add files via upload

--- import csvtest.py
+++ import csvtest.py
@@ -11,19 +11,28 @@ y = []
 
 
 G = nx.Graph()
+
+#lijnen tussen staten
 with open('neighborsstates.csv', 'r') as csvfile:
 	plots = csv.reader(csvfile, delimiter=',')	
 	for row in plots:
 		G.add_edge(str(row[0]),str(row[1]))
-		# G.append(str(row[1]))
+		
+		fixed_positions = (str(row[0]):(int(row[2])),(int(row[3])))
 
-print(G)
+		fixed_nodes = fixed_positions.keys()
+
+		pos = nx.spring_layout(G,pos=fixed_positions, fixed = fixed_nodes)
+
+		nx.draw_networkx(G,pos, with_labels=True)
+
+
+
+
+
 
 print nx.info(G)
 
-nx.draw(G)
+#nx.draw(G, with_labels=True)
 
 plt.show()
-
-
-
