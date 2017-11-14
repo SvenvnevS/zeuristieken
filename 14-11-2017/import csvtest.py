62aed7bd2a6b97b721f9830d6baaa6c7a3d465ec
SvenvBoven <sven_vboven@hotmail.com>
2017-11-14 15:55:48 +0100

    zelfde weer

--- 14-11-2017/import csvtest.py
+++ 14-11-2017/import csvtest.py
@@ -6,43 +6,72 @@ import random
 from networkx.algorithms import bipartite
 
 
-getalreeks = []
-
-G = nx.Graph()
-
-# nodes neerzetten per provincie
-with open('nodes.csv', 'r') as csvfile:
-	plots = csv.reader(csvfile, delimiter=',')	
-	for row in plots:
-		G.add_node(str(row[0]))
+def main():
 
 
-# fixed_positions = {str(row[2]):((int(row[3])),(int(row[4])))}
+	G = nx.Graph()
 
-# fixed_nodes = fixed_positions.keys()
-
-# pos = nx.spring_layout(G, pos=fixed_positions, fixed = fixed_nodes)
-
-# # lijnen tussen staten
-with open('edges.csv', 'r') as csvfile:
-	plots = csv.reader(csvfile, delimiter=',')	
-	for row in plots:
-		G.add_edge(str(row[0]),str(row[1]))
-
-for x in range(26):
-	getal = random.randint(1,7)
-	getalreeks.append(getal)
-print (getalreeks)
+	# nodes neerzetten per provincie
+	with open('nodes.csv', 'r') as csvfile:
+		plots = csv.reader(csvfile, delimiter=',')	
+		for row in plots:
+			G.add_node(str(row[2]),color=createColor())
 
 
-with open(OUTPUT_CSV, 'wb') as output_file:
-writer = csv.writer(f)
-	
+	# # lijnen tussen staten
+	with open('oekraineprovincies.csv', 'r') as csvfile:
+		plots = csv.reader(csvfile, delimiter=',')	
+		for row in plots:
+			G.add_edge(str(row[0]),str(row[1]))
+		
 
 
+	# red green blue yellow orange purple grey
+	score=0
+	colormap = []
+	print(nx.info(G))
+	for node in G.nodes():
+		# print(G.nodes[node])
+		color=nx.get_node_attributes(G,'color')
+		print(color[node])
+		colormap.append(color[node])		
+		# nx.draw_networkx(G.nodes[node], with_labels=True,node_color=coloor[node])
+	for kleur in colormap:
+		if kleur == 'red':
+			score += 12
+		if kleur == 'green':
+			score += 26
+		if kleur == 'blue':
+			score += 27
+		if kleur == 'yellow':
+			score += 30
+		if kleur == 'orange':
+			score += 37
+		if kleur == 'purple':
+			score += 39
+		if kleur == 'grey':
+			score += 41
+	print("score is: {}".format(score))
+	nx.draw_networkx(G, with_labels=True,node_color=colormap)
 
-print(nx.info(G))
+	plt.show()
 
-nx.draw_networkx(G, with_labels=True)
+def createColor():
+	i = random.randint(1,7)
+	if i is 1:
+		return 'red'
+	if i is 2:
+		return 'green'
+	if i is 3:
+		return 'blue'
+	if i is 4:
+		return 'yellow'
+	if i is 5:
+		return 'orange'
+	if i is 6:
+		return 'purple'
+	if i is 7:
+		return 'grey'
 
-plt.show()
+if __name__ == '__main__':
+	main()
