c9715fcbf85d9d52f0f07f767785a45bd1eb1dd5
SvenvBoven <sven_vboven@hotmail.com>
2017-11-14 10:24:12 +0100

    hier bob

--- import csvtest.py
+++ import csvtest.py
@@ -1,5 +1,5 @@
 import csv
-import StringIO
+import io
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.algorithms import bipartite
@@ -8,10 +8,17 @@ from networkx.algorithms import bipartite
 G = nx.Graph()
 
 # nodes neerzetten per provincie
-with open('oekraineprovincies.csv', 'r') as csvfile:
+with open('nodes.csv', 'r') as csvfile:
 	plots = csv.reader(csvfile, delimiter=',')	
 	for row in plots:
 		G.add_node(str(row[2]))
+
+
+fixed_positions = {str(row[2]):((int(row[3])),(int(row[4])))}
+
+fixed_nodes = fixed_positions.keys()
+
+pos = nx.spring_layout(G, pos=fixed_positions, fixed = fixed_nodes)
 
 # # lijnen tussen staten
 with open('oekraineprovincies.csv', 'r') as csvfile:
@@ -19,15 +26,10 @@ with open('oekraineprovincies.csv', 'r') as csvfile:
 	for row in plots:
 		G.add_edge(str(row[0]),str(row[1]))
 	
-# fixed_positions = {str(row[2]):((int(row[3])),(int(row[4])))}
-
-# fixed_nodes = fixed_positions.keys()
-
-# pos = nx.spring_layout(G, pos=fixed_positions, fixed = fixed_nodes)
 
 
 
-print nx.info(G)
+print(nx.info(G))
 
 nx.draw_networkx(G, with_labels=True)
 
