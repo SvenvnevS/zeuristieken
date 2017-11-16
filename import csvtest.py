2017ae49789cc3eb8adafa2cc9751ba312f1e267
SvenvBoven <sven_vboven@hotmail.com>
2017-11-16 13:11:01 +0100

    dode code verwijdert

--- import csvtest.py
+++ import csvtest.py
@@ -16,7 +16,7 @@ def main():
 			G.add_node(str(row[0]),color='None')
 
 
-	# # lijnen tussen staten
+	# lijnen tussen staten
 	with open('edges.csv', 'r') as csvfile:
 		plots = csv.reader(csvfile, delimiter=',')	
 		for row in plots:
@@ -25,16 +25,12 @@ def main():
 	for node in G.nodes():
 		createColor(G, node, controleColor(G, node))
 
-	# red green blue yellow orange purple grey
 	score=0
 	colormap = []
 	print(nx.info(G))
 	for node in G.nodes():
-		# print(G.nodes[node])
 		color=nx.get_node_attributes(G,'color')
-		# print(color[node])
 		colormap.append(color[node])		
-		# nx.draw_networkx(G.nodes[node], with_labels=True,node_color=coloor[node])
 	for kleur in colormap:
 		if kleur == 'red':
 			score += 12
@@ -52,9 +48,7 @@ def main():
 			score += 41
 	print("score is: {}".format(score))
 
-	# print(G.nodes())
-	print(G. node["Kiev"])
-	# print(G["Kiev"])
+
 	nx.draw_networkx(G, with_labels=True,node_color=colormap)
 
 	plt.show()
